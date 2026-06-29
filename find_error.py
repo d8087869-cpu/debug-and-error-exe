7b203f90225ecf0973832e08ffab3ddab9ac0497
david <d8087869@gmail.com>
2026-06-29 16:00:47 +0300

    added2-3

--- find_error.py
+++ find_error.py
@@ -6,3 +6,21 @@ except ValueError:
     print('age must be a number')
     exit()
 print("Next year you will be", next_year)
+#2 
+try:
+    a = int(input("First number: ")) 
+    b = int(input("Second number: ")) 
+    print(a / b) 
+except ZeroDivisionError:
+    print('cannot divide by zero')
+except ValueError:
+    print('enter int or flaot ')    
+#3 
+try:
+    numbers = [10, 20, 30] 
+    index = int(input("Choose index: ")) 
+    print(numbers[index])
+except IndexError:
+    print('index not found ')   
+except ValueError:
+    print('enter only int numbers ')    
